32a3359074b3aa88224f7aecbb45b550b2a54e5a
Bharathikva <sundarabharathi996@gmail.com>
2024-12-31 12:06:48 +0530

    add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { CelebrationComponent } from './celebration/celebration.component';
 
 export const routes: Routes = [
-    { path: 'celebration', component: CelebrationComponent },
-    { path: '**', redirectTo: 'celebration' },
+    // { path: 'celebration', component: CelebrationComponent },
+    // { path: '**', redirectTo: 'celebration' },
   ];
